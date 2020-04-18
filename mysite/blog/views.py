b5241e1a0afc0d1d882523c479f621aafd554fef
IggorsBot <IggorsBot@gmail.com>
2020-04-18 13:20:57 +0300

    add comment system

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, \
                                   PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
-# from django.core.mail import send_mail
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -37,14 +37,36 @@ def post_detail(request, year, month, day, post):
                                     publish__month=month,
                                     publish__day=day)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active = True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html',
+                           {'post': post,
+                           'comments': comments,
+                           'new_comment': new_comment,
+                           'comment_form': comment_form})
 
 
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_objects_or_404(Post, id=post_id, status='published')
     sent = False
-    if request.method == 'POST'
+    if request.method == 'POST':
         # Form fields passed validation
         cd = form.cleaned_data
         # ... send data
